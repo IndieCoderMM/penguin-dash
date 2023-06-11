9a51886556d52361d9ce2cc03dfed68d61653dba
hein thant <hthant00chk@gmail.com>
2023-06-11 23:09:23 +0630

    Config for deploy

--- parcel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Specify the entry point of your application
-  entry: 'src/main.ts',
-
-  // Add a transformer for TypeScript files
-  transform: {
-    '^.+\\.ts$': 'tsify',
-  },
-};
